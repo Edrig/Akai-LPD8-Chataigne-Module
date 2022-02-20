ee1bacbb12e851346a02d6715cec40d9e1cd7e52
Noel Gaetan <gaetan.noel@viacesi.fr>
2022-02-21 00:32:52 +0100

    add feature
    + Pad Colors
    + Values Pad
    + Knobs

--- AkaiLPD8.js
+++ AkaiLPD8.js
@@ -1,11 +1,7 @@
-
-//local.sendNoteOn(1, C0, 2);
-
- //Functions
+//Functions
 
 function setLed(pad, color)
 {
-	script.log("SetLed:  "+pad+"/"+colors[0]);
 	local.sendNoteOn(1, pad+35, colors[0]);
 }
 
@@ -14,38 +10,64 @@ function setLed(pad, color)
 
 function setPadColor(pad, colors)
 {
-	setLed(pad, color[0]);
+	//setLed(pad, color[0]);
+	i = parseInt(pad);
+	//script.log("a: "+i+"/"+colors[0]);
+	x = colors[0];
+	if (x == 0)
+	{
+		local.values.padColors.getChild("pad"+i).set("Black");
+	}
+	else if (x == 127)
+	{
+		local.values.padColors.getChild("pad"+i).set("Orange");
+	}
 }
+	
+
 
 function resetColors()
 {
-	for(var i=0;i<63;i++) setLed(i, 0);;
-}
+	for(var i=0;i<63;i++) 
+		{
+			setPadColor(i, 0);
+		}
 
+}
 
 //Events
 
 function moduleParameterChanged(param)
 {
-	if(param.getParent().name == "padColors")
-	{
-		var id = parseInt(param.name.substring(3));
-		var val = param.get();
-		script.log("Test: "+val[0]+"/"+val[1]);
-		//setLed(id, val[0],val[1], 0, 0);
-	}
+  script.log(value.name + " param changed, new value: " + param.get());
 }
 
+function moduleValueChanged(value) {
+  script.log(value.name + " value changed, new value: " + value.get());
+  	if(value.getParent().name == "padColors")
+	{
+		var id = parseInt(value.name.substring(3, 5));
+		var val = value.get();
+		script.log("Test: "+id+"/"+value.name.substring(3, 5)+"/"+val[0]);
+		//setLed(value.name.substring(3, 5), val[0]);
+		local.sendNoteOn(1, id+35, val[0]);
+
+	}
+}
 
 function noteOnEvent(channel, pitch, velocity)
 {
 	script.log("Note on received "+channel+", "+pitch+", "+velocity);
+	i = pitch - 35;
+    local.values.pads.getChild("pad" + i).set(1);
 }
 
 
 function noteOffEvent(channel, pitch, velocity)
 {
 	script.log("Note off received "+channel+", "+pitch+", "+velocity);
+	i = pitch - 35;
+    local.values.pads.getChild("pad" + i).set(0);
 }
 
 function ccEvent(channel, number, value)
